ecd3e6aa1e6338bcb7f62f53232af61958526868
nicolas.bern <nicolas.bern@epsi.fr>
2021-02-10 22:36:05 +0100

    Afficher infos album

--- src/views/Album.vue
+++ src/views/Album.vue
@@ -1,11 +1,30 @@
 <template>
-  <div class="album">
-
+  <div id="album">
     <router-link :to="'/'">
-      <button>RETOUR</button>
+      <button class="retour">RETOUR</button>
     </router-link>
 
-    <h1>{{ album.title }}</h1>
+    <h1>{{ album.title }} by {{ album.artists['0'].name }}</h1>
+
+    <h2>Tracklist :</h2>
+
+    <table v-for="(track, index) in tracklist">
+      <tbody>
+        <tr>
+          <td>
+            {{ index = index+1 }}) {{ track.title }} | {{ track.duration }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+    <h3>Style(s) :</h3>
+    <span v-for="(style, index) in styles">{{ style }}
+      <span v-if="!isLast(index, styles)">, </span>
+    </span>
+
+
+    <h4>Year : {{ album.year }}</h4>
 
   </div>
 </template>
@@ -17,7 +36,11 @@ export default {
   name: "Album",
 
   data(){
-    album: {}
+    return{
+      album: {},
+      tracklist: {},
+      styles: {}
+    }
   },
 
   created() {
@@ -31,13 +54,16 @@ export default {
         url: 'https://api.discogs.com/masters/' + this.$route.params.id
       };
 
-      console.log(options.url)
-      console.log("id : " + this.$route.params.id)
       axios.request(options).then(res =>{
         this.album = res.data
+        this.tracklist = this.album.tracklist
+        this.styles = this.album.styles
         console.log(this.album)
-        console.log(this.album.title)
       })
+    },
+
+    isLast: function (index, list){
+      return list.length === index + 1
     }
   }
 
